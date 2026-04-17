4d3e19c9d9755995cfc5101107456aae10ec8d3e
whypikmi <ttelatnikova1@yandex.ru>
2026-04-17 15:38:07 +0300

    Extract StyledText into a separate widget and file

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lab3_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -16,15 +17,7 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomCenter,
         ),
       ),
-      child: Center(
-        child: Text(
-          "Hello world!",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 32,
-          ),
-        ),
-      ),
+      child: Center(child: StyledText()),
     );
   }
 }
