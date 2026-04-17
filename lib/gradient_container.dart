81f72aaa8752b2fa5aa69e0afc62ed91ea702031
whypikmi <ttelatnikova1@yandex.ru>
2026-04-17 16:00:17 +0300

    Style roll button and adjust layout

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -27,10 +27,20 @@ class GradientContainer extends StatelessWidget {
       ),
       child: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset('dice-1.png', width: 300),
             TextButton(
               onPressed: rollDice,
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.only(
+                  top: 20,
+                ),
+                foregroundColor: Colors.lime,
+                textStyle: const TextStyle(
+                  fontSize: 30,
+                ),
+              ),
               child: Text("Roll Dice"),
             ),
           ],
